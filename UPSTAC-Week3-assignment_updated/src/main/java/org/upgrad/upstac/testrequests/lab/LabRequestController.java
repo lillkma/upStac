c032255eee3684a98ba309cb33c2b0a579d7d1f1
Lillian Mani Paul Kongu <Lillian_Kongu@ad.infosys.com>
2020-12-04 23:52:38 +0530

    Formatted code.

--- UPSTAC-Week3-assignment_updated/src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
+++ UPSTAC-Week3-assignment_updated/src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
@@ -71,7 +71,7 @@ public class LabRequestController {
         //Implement this method to return the list of test requests assigned to current tester(make use of the above created User object)
         //Make use of the findByTester() method from testRequestQueryService class
         // For reference check the method getForTests() method from LabRequestController class
-        User tester =userLoggedInService.getLoggedInUser();
+        User tester = userLoggedInService.getLoggedInUser();
         return testRequestQueryService.findByTester(tester);
 
     }
@@ -81,11 +81,8 @@ public class LabRequestController {
     @PutMapping("/assign/{id}")
     public TestRequest assignForLabTest(@PathVariable Long id) {
 
-
-
         User tester =userLoggedInService.getLoggedInUser();
-
-      return   testRequestUpdateService.assignForLabTest(id,tester);
+        return   testRequestUpdateService.assignForLabTest(id,tester);
     }
 
     @PreAuthorize("hasAnyRole('TESTER')")
